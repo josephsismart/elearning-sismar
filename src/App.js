5490953f21ab51c6efd51f1c7797e95d39c4b272
Joseph Sismar <30491006+josephsismart@users.noreply.github.com>
2026-07-27 20:45:29 +0800

    Refactor App component to include Navbar and pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Schedule from './components/Schedule';
+
+function App() {
+  const [page, setPage] = useState('home');
+  return (
+    <div>
+      <Navbar current={page} onNav={setPage} />
+      {page === 'home' && <Home />}
+      {page === 'schedule' && <Schedule />}
+    </div>
+  );
+}
+
+export default App;import React, { useState } from 'react';
 import './index.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
