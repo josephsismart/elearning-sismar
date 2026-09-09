81d3ce0b0cd3fba4b5572080c79e3eca620826bc
Joseph Sismar <30491006+josephsismart@users.noreply.github.com>
2026-09-09 22:07:24 +0800

    Update App.js with SF9 component imports and routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Schedule from './components/Schedule';
+import SchoolYear from './components/SchoolYear';
+import Enrollment from './components/Enrollment';
+import AttendanceSF9 from './components/AttendanceSF9';
+import Grades from './components/Grades';
+import SF9Report from './components/SF9Report';
 
 function App() {
   const [page, setPage] = useState('home');
@@ -10,6 +15,11 @@ function App() {
       <Navbar current={page} onNav={setPage} />
       {page === 'home' && <Home />}
       {page === 'schedule' && <Schedule />}
+      {page === 'schoolyear' && <SchoolYear onNav={setPage} />}
+      {page === 'enrollment' && <Enrollment onNav={setPage} />}
+      {page === 'attendance' && <AttendanceSF9 onNav={setPage} />}
+      {page === 'grades' && <Grades onNav={setPage} />}
+      {page === 'sf9report' && <SF9Report onNav={setPage} />}
     </div>
   );
 }
